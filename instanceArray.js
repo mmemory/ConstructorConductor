d0954313dacb7894e25e4e4f89df1ae3a62d86a3
Michael Memory <michaelmemoria@gmail.com>
2015-04-07 15:32:53 -0600

    instanceArray Problem

--- instanceArray.js
+++ instanceArray.js
@@ -1,41 +1,62 @@
 /*
   Your 3 Users will be the following. 
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
-    1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
-    2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
+    1) 'Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags'
+    2) 'Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup'
 */
 
 var User = function(name, email, pw){
   this.name = name;
   this.email = email;
   this.pw = pw;
-}
+};
 
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
+var users = [];
 
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
   //code here
+var user1 = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+var user2 = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+var user3 = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
 
-console.log('Tyler\'s information is ');
+users.push(user1, user2, user3);
+
+//console.log('Tyler\'s information is', 'Name:', users[0].name, 'Email:', users[0].email, 'Password:', users[0].pw);
 //Console.log all of Tylers information
 
-  //code here
 
-console.log('Lenny\'s information is ');
+//console.log('Lenny\'s information is', 'Name:', users[1].name, 'Email:', users[1].email, 'Password:', users[1].pw);
 //Now console.log all of Lennys information
 
-  //code here
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+var me = new User('Michael', 'michael@gmail.com', 'pass');
+users.push(me);
 
-console.log('All my users names are ');
+
+
+var userLoop = function (arr, cb) {
+    var nameArray = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        nameArray.push(arr[i].name);
+    }
+    return cb(nameArray);
+};
+
+userLoop(users, function (user) {
+    var names = user.join(', ');
+    console.log('All my users names are', names);
+});
+
+
 //Now loop through your users Array and console.log every users name. 
 
-  //code here
+
